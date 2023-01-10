049f84b316e8172b4ea9000e13cdf818a4a9f508
msong16 <maysong168@gmail.com>
2023-01-10 01:51:34 -0500

    Add filters

--- src/components/Tableau.js
+++ src/components/Tableau.js
@@ -1,13 +1,8 @@
 import React, {useEffect, useRef} from 'react';
 import tableau from 'tableau-api';
 
-//const {tableau}  = window;
 export const Tableau = ()=>{
-    //const {tableau} = window;
-    
-    // useEffect(()=>{
-    //     initViz();
-    // },[])
+
     var viz;
     const containerDiv = document.getElementById("vizContainer");
     const vizUrl = "https://public.tableau.com/views/Superstore_24/Overview";
@@ -16,22 +11,36 @@ export const Tableau = ()=>{
     if(existingViz){
         existingViz.dispose();
     }
-     viz = new window.tableau.Viz(containerDiv, vizUrl)
-    
-    //const ref = useRef(null);
-    const options = {}
-    
-    // const initViz = () =>{
-    //     const containerDiv = document.getElementById("container")
-    //     const viz = new window.tableau.Viz(containerDiv, vizUrl)
-    // }
+    const options = {
+        "State": "Pennsylvania",
+        "Order Date": ""
+    }
+    /*https://community.tableau.com/s/question/0D54T00000CWes9SAD/how-to-combine-applyrangefilterasync-and-applyfilterasync
+    https://community.tableau.com/s/question/0D54T000006B4HpSAK/apply-a-range-filter-to-a-dashboard-worksheet-before-loading-the-visualization
+    https://www.npmjs.com/package/tableau-api
+    function applyDateFilter(startDate, endDate, target) {
+        sheet = target.getWorkbook().getActiveSheet();
 
-    //const viz = new window.tableau.Viz(ref, vizUrl, options);
+        var filterOptions = {
+            min: startDate,
+            max: endDate
+        };
+    if (sheet.getSheetType() === 'worksheet') {
+        sheet.applyRangeFilterAsync("Action Date", filterOptions);
+    } else {
+        worksheetArray = sheet.getWorksheets();
+
+        for (var i = 0; i < worksheetArray.length; i++) {
+            worksheetArray[i].applyRangeFilterAsync("Action Date", filterOptions);
+        }
+    }
+}
+*/
+     viz = new window.tableau.Viz(containerDiv, vizUrl, options)
 
     return(
         <div>
             <h2>Embedded Tableau</h2>
-            {/* <div ref={ref} style={{width:'70%', margin:'auto'}}></div> */}
         </div>
         
     )
