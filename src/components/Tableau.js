d22f63112fd6a2c6a81ae378de7e9b99b0f4c5f4
msong16 <maysong168@gmail.com>
2023-01-12 09:59:06 -0500

    Add styling

--- src/components/Tableau.js
+++ src/components/Tableau.js
@@ -1,7 +1,12 @@
 import React, {useEffect, useRef} from 'react';
 import tableau from 'tableau-api';
 import useExternalScripts from '../hooks/useExternalScripts';
-import { Helmet } from "react-helmet"
+import { Helmet } from "react-helmet";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import 'bootstrap/dist/css/bootstrap.css'
 export const Tableau = ()=>{
 
    // var viz;
@@ -46,15 +51,18 @@ export const Tableau = ()=>{
     //     console.log(sheet)
     //  }
     //  if (sheet){
-    //     overview = sheet.worksheets.find(ws => ws.name =="Overview");
+    //     overview = sheet.worksheets.find(ws => ws.name =="SaleMap");
     //  }
     //  if (overview){
     //     overview.applyRangeFilterAsync("Order Date", {
-    //         min: new Date(Date.UTC(2012, 3, 1)),
+    //         min: new Date(Date.UTC(2013, 3, 1)),
     //         max: new Date(Date.UTC(2020, 8, 30))
     //    });
     //  }
      
+     useEffect(()=>{
+
+     },[])
      const useExternalScript = () => {
         useExternalScripts("https://public.tableau.com/javascripts/api/tableau.embedding.3.latest.min.js")
     }
@@ -70,13 +78,29 @@ export const Tableau = ()=>{
           async
         ></script>
       </Helmet>
-            <h2>Embedded Tableau</h2>
-            <tableau-viz id="tableauViz"       
-            src='https://public.tableau.com/views/Superstore_24/Overview'      
-            toolbar="bottom" height="800px" width="100%" style={{}}>
-                <viz-filter field="State" value="Florida"> </viz-filter>    
-            </tableau-viz>
-            <h3>footer</h3>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+        <Navbar.Brand href="#home">
+            React App
+          </Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Menu Item 1</Nav.Link>
+            <Nav.Link href="#pricing">Menu Item 2</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+            <h2 style={{marginLeft:'5%', marginTop:'2%', marginBottom:'2%'}}>Embedded Tableau</h2>
+            <div style={{display:'flex', alignItems:'center', 
+            justifyContent:'center'}}>
+                <tableau-viz id="tableauViz"       
+                src='https://public.tableau.com/views/Superstore_24/Overview'      
+                toolbar="bottom" width="100%" style={{}}>
+                    <viz-filter field="State" value="Florida"> </viz-filter>  
+                </tableau-viz>
+            </div>
+            
+            
         </div>
         
     )
